927a8b9a6e86e85a7deb86a89eb9853383053511
Vyacheslav Morev <gromd96@gmail.com>
2023-02-09 01:42:54 +0300

    сделать открытие и закрытие попапа js

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+let profile = document.querySelector('.profile');
+let profileEditButton = profile.querySelector('.profile__edit-button');
+let profileTitle = profile.querySelector('.profile__title');
+let profileSubtitle = profile.querySelector('.profile__subtitle');
+let popup = document.querySelector('.popup');
+let popupCloseButton = popup.querySelector('.popup__close-button');
+let formElement = popup.querySelector('.popup__form');
+let nameInput = popup.querySelector('.popup__text_input_name');
+let jobInput = popup.querySelector('.popup__text_input_job');
+
+profileEditButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  popup.classList.add('popup_opened');
+});
+
+popupCloseButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  popup.classList.remove('popup_opened');
+});
+
+formElement.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  popup.classList.remove('popup_opened');
+});
