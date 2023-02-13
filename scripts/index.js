0d7c4b076a6e235c10fc3d862d4c8bb9af2a8309
Vyacheslav Morev <gromd96@gmail.com>
2023-02-13 16:44:42 +0300

    реализовать загрузку галереи фотографий через js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,56 @@
-let profile = document.querySelector('.profile');
-let profileEditButton = profile.querySelector('.profile__edit-button');
-let profileTitle = profile.querySelector('.profile__title');
-let profileSubtitle = profile.querySelector('.profile__subtitle');
-let popup = document.querySelector('.popup');
-let popupCloseButton = popup.querySelector('.popup__close-button');
-let formElement = popup.querySelector('.popup__form');
-let nameInput = popup.querySelector('.popup__text_input_name');
-let jobInput = popup.querySelector('.popup__text_input_job');
+const profile = document.querySelector('.profile');
+const profileEditButton = profile.querySelector('.profile__edit-button');
+const profileTitle = profile.querySelector('.profile__title');
+const profileSubtitle = profile.querySelector('.profile__subtitle');
+const popup = document.querySelector('.popup');
+const popupCloseButton = popup.querySelector('.popup__close-button');
+const formElement = popup.querySelector('.popup__form');
+const nameInput = popup.querySelector('.popup__text_input_name');
+const jobInput = popup.querySelector('.popup__text_input_job');
+const cardTemplate = document.querySelector('#card').content;
+const cardsList = document.querySelector('.cards__list');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    alternative: 'Фотография гор Архыза'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    alternative: 'Фотография заснеженного озера в Челябинской области'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    alternative: 'Фотография девятиэтажных домов в Иваново'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    alternative: 'Фотография горы на Камчатке'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    alternative: 'Фотография железной дороги посреди леса'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    alternative: 'Фотография утёса на Байкале'
+  }
+];
+
+const galleryCard = initialCards.forEach(card => {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__image').alt = card.alternative;
+  cardElement.querySelector('.card__text').textContent = card.name;
+  cardsList.append(cardElement);
+})
+
 
 profileEditButton.addEventListener('click', () => {
   popup.classList.add('popup_opened');
