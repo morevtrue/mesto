1a6a8a05010605d72f938f739f6a9aaaaa0ddf23
Vyacheslav Morev <gromd96@gmail.com>
2023-02-25 18:36:29 +0300

    реализовать лайк карточки

--- scripts/index.js
+++ scripts/index.js
@@ -12,12 +12,13 @@ const jobInput = popupEditProfile.querySelector('.popup__text_input_job');
 // ПОПАП ДОБАВИТЬ КАРТОЧКУ
 const popupAddCard = document.querySelector('.popup_add_card');
 const closePopupButtonAddCard = popupAddCard.querySelector('.popup__close-button');
-const namePlaceInput = popupAddCard.querySelector('.popup__text_input_name');
-const srcImageInput = popupAddCard.querySelector('.popup__text_input_job');
+const namePlaceInput = popupAddCard.querySelector('.popup__text_input_place');
+const srcImageInput = popupAddCard.querySelector('.popup__text_input_src');
 const formElementCard = popupAddCard.querySelector('.popup__form');
 
 const cardTemplate = document.querySelector('#card').content;
 const cardsList = document.querySelector('.cards__list');
+const cardLike = document.querySelectorAll('.card__like');
 // const cardName = cardTemplate.querySelector('.card__text');
 // const cardImage = cardTemplate.querySelector('.card__image');
 
@@ -25,41 +26,57 @@ const initialCards = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-    alternative: 'Фотография гор Архыза'
   },
   {
     name: 'Челябинская область',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-    alternative: 'Фотография заснеженного озера в Челябинской области'
   },
   {
     name: 'Иваново',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-    alternative: 'Фотография девятиэтажных домов в Иваново'
   },
   {
     name: 'Камчатка',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-    alternative: 'Фотография горы на Камчатке'
   },
   {
     name: 'Холмогорский район',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-    alternative: 'Фотография железной дороги посреди леса'
   },
   {
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-    alternative: 'Фотография утёса на Байкале'
   }
 ];
+
+// ОТКРЫТЬ ПОПАП
+
+function openPopup(currentPopup) {
+  currentPopup.classList.add('popup_opened');
+};
+
+// ЗАКРЫТЬ ПОПАП
+
+function closePopup(currentPopup, closePopupButton) {
+  closePopupButton.addEventListener('click', () => {
+    currentPopup.classList.remove('popup_opened');
+  });
+}
+
+// ЛАЙКНУТЬ КАРТОЧКУ
+
+function likeButton(currentElement) {
+  currentElement.querySelector('.card__like').addEventListener('click', evt => {
+    evt.target.classList.toggle('card__like_active');
+  })
+}
 
 // ЗАГРУЗКА ГАЛЕРЕИ ФОТОГРАФИЙ НА СТРАНИЦУ
 
 const galleryCard = initialCards.forEach(card => {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  likeButton(cardElement);
   cardElement.querySelector('.card__image').src = card.link;
-  cardElement.querySelector('.card__image').alt = card.alternative;
   cardElement.querySelector('.card__text').textContent = card.name;
   cardsList.append(cardElement);
 });
@@ -67,16 +84,13 @@ const galleryCard = initialCards.forEach(card => {
 // ПОПАП ПРОФИЛЬ
 
 openPopupProfile.addEventListener('click', () => {
-  popupEditProfile.classList.add('popup_opened');
+  openPopup(popupEditProfile);
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
   // popup.style.display = 'flex';
 });
 
-closePopupButtonProfile.addEventListener('click', () => {
-  popupEditProfile.classList.remove('popup_opened');
-  // popup.style.display = 'none';
-});
+closePopup(popupEditProfile, closePopupButtonProfile);
 
 formElementProfile.addEventListener('submit', evt => {
   evt.preventDefault();
@@ -89,25 +103,25 @@ formElementProfile.addEventListener('submit', evt => {
 // ПОПАП ДОБАВИТЬ КАРТОЧКУ
 
 openPopupAddCard.addEventListener('click', () => {
-  popupAddCard.classList.add('popup_opened');
-  namePlaceInput.value = 'Название';
-  srcImageInput.value = 'Ссылка на картинку';
-  // popup.style.display = 'flex';
+  openPopup(popupAddCard);
 });
 
-closePopupButtonAddCard.addEventListener('click', () => {
-  popupAddCard.classList.remove('popup_opened');
-  // popup.style.display = 'none';
-});
+closePopup(popupAddCard, closePopupButtonAddCard);
 
 formElementCard.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  likeButton(cardElement);
   cardElement.querySelector('.card__text').textContent = namePlaceInput.value;
   cardElement.querySelector('.card__image').src = srcImageInput.value;
-  cardsList.append(cardElement);
+  cardsList.prepend(cardElement);
   popupAddCard.classList.remove('popup_opened');
+  namePlaceInput.value = '';
+  srcImageInput.value = '';
 });
+
+
+
 
 
 
