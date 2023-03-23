dd0964e3fc25ce49149758f34527d6015b7c4bda
Vyacheslav Morev <gromd96@gmail.com>
2023-03-23 16:48:00 +0300

    удалить неиспользуемые переменные в js

--- scripts/index.js
+++ scripts/index.js
@@ -5,13 +5,11 @@ const profileTitle = profile.querySelector('.profile__title');
 const profileSubtitle = profile.querySelector('.profile__subtitle');
 const cardsList = document.querySelector('.cards__list');
 const popupList = document.querySelectorAll('.popup');
-const popupCloseButtons = document.querySelectorAll('.popup__close-button');
 // ПОПАП ПРОФИЛЬ-----------------------------------------
 const popupEditProfile = document.querySelector('.popup_edit_profile');
 const formElementProfile = popupEditProfile.querySelector('.popup__form');
 const nameInput = popupEditProfile.querySelector('.popup__text_input_name');
 const jobInput = popupEditProfile.querySelector('.popup__text_input_job');
-const buttonClosePopupProfile = popupEditProfile.querySelector('.popup__close-button');
 const popupProfileInputList = popupEditProfile.querySelectorAll('.popup__form-input');
 const popupProfileSubmitButton = popupEditProfile.querySelector('.popup__submit-button');
 // ПОПАП ДОБАВИТЬ КАРТОЧКУ---------------------------------
@@ -19,14 +17,12 @@ const popupAddCard = document.querySelector('.popup_add_card');
 const namePlaceInput = popupAddCard.querySelector('.popup__text_input_place');
 const srcImageInput = popupAddCard.querySelector('.popup__text_input_src');
 const formElementCard = popupAddCard.querySelector('.popup__form');
-const buttonClosePopupCard = popupAddCard.querySelector('.popup__close-button');
 const popupCardInputList = popupAddCard.querySelectorAll('.popup__form-input');
 const popupCardSubmitButton = popupAddCard.querySelector('.popup__submit-button');
 // ПОПАП ПОСМОТРЕТЬ ФОТОГРАФИЮ------------------------------
 const popupViewPhoto = document.querySelector('.popup_open_image');
 const popupImageText = popupViewPhoto.querySelector('.popup__image-text');
 const popupImage = popupViewPhoto.querySelector('.popup__image');
-const buttonClosePopupPhoto = popupViewPhoto.querySelector('.popup__close-button')
 //РАБОТА С ЭЛЕМЕНТОМ TEMPLATE--------------------------------
 const cardTemplate = document.querySelector('#card').content;
 const cardElement = cardTemplate.querySelector('.card');
@@ -154,5 +150,3 @@ function submitFormAddCard(evt) {
   evt.target.reset();
   closePopup(popupAddCard);
 };
-
-
